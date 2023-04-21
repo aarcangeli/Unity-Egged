de41abfabb43f7587bd37628d0e73b85094c940b
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2023-04-21 22:47:05 +0200

    flag

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Goal : MonoBehaviour
+{
+	public GameObject RotatableFlag;
+
+	private GameObject _player;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_player = GameObject.FindWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// make rotation
+		if (!RotatableFlag) return;
+		
+		var delta = _player.transform.position - RotatableFlag.transform.position;
+		delta.y = 0;
+		delta.Normalize();
+		var rotation = Quaternion.LookRotation(delta);
+		print(rotation.eulerAngles);
+
+		var rotationEulerAngles = RotatableFlag.transform.rotation.eulerAngles;
+		RotatableFlag.transform.rotation = rotation;
+	}
+}
